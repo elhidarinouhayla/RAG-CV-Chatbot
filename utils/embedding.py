51604105364c09e5a0c927d01998bc76213383fa
elhidarinouhayla <elhidarinouhayla@gmail.com>
2026-01-31 00:15:01 +0100

    implement RAG chain with Gemini Flash and explicit API auth

--- utils/embedding.py
+++ utils/embedding.py
@@ -2,37 +2,62 @@ from langchain.embeddings import HuggingFaceEmbeddings
 from pdf_loading import load_pdf
 from text_splitter import split_pages
 from langchain_community.vectorstores import Chroma
+import os
+from langchain.chains import RetrievalQA
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_community.vectorstores import Chroma
+from config import GEMINI_API_KEY
 
 
-pdf_path = "C:/Users/hp/desktop/RAG-CV-Chatbot/data/pdf/data.pdf"
+pdf_path = "C:/Users/hp/desktop/RAG-CV-Chatbot/data/pdf/cv.pdf"
 
 pages = load_pdf(pdf_path)
 chunks = split_pages(pages)
 
 
+def save_vectordb():
+
+    embedding_model = HuggingFaceEmbeddings(
+        model_name = "sentence-transformers/all-MiniLM-L6-v2"
+    )
+
+    texts = [chunk.page_content for chunk in chunks]
 
 
-embedding_model = HuggingFaceEmbeddings(
-    model_name = "sentence-transformers/all-MiniLM-L6-v2"
-)
-
-texts = [chunk.page_content for chunk in chunks]
+    embeddings = embedding_model.embed_documents(texts)
 
 
-embeddings = embedding_model.embed_documents(texts)
+    # Save to a vector db
+    vector_db = Chroma.from_documents(
+        documents=chunks,
+        embedding=embedding_model,
+        persist_directory ="../data/vectordb/chroma_db"
+    ) 
 
-print(f"number of texts : {len(texts)}")
-print(f"number of embeddings : {len(embeddings)}")
+    return vector_db
+ 
 
 
 
+def configuration(vector_db):
+    llm = ChatGoogleGenerativeAI(
+        model="gemini-flash-latest",
+        temperature=0,
+        google_api_key=GEMINI_API_KEY
+        )
+    
+    retr = RetrievalQA.from_chain_type(
+        llm=llm,
+        chain_type="stuff",
+        retriever=vector_db.as_retriever(search_kwargs={"k": 3})
+    )
+    return retr
 
-# Save to a vector db
-vector_db = Chroma.from_documents(
-    documents=chunks,
-    embedding=embedding_model,
-    persist_directory ="../data/vectordb/chroma_db"
-) 
+db = save_vectordb()
+reponse = configuration(db)
 
-vector_db.persist()
-print("vector db created")
+question = "Quelles sont les coordonnees ou les informations de contact mentionnees dans le document ?"
+answer = reponse.invoke({"query": question})
+
+print("chatbot response :")
+print(answer["result"])
